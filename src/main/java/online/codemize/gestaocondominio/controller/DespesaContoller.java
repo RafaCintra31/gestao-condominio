04e28a5910f7c8ddd65a4fbfeecbf566a9509a6b
rafa cintra <rafa.cintrati@gamil.com>
2025-06-27 14:57:03 -0300

    atualizacao do projeto

--- src/main/java/online/codemize/gestaocondominio/controller/DespesaContoller.java
+++ src/main/java/online/codemize/gestaocondominio/controller/DespesaContoller.java
@@ -1,5 +1,6 @@
 package online.codemize.gestaocondominio.controller;
 
+import lombok.RequiredArgsConstructor;
 import online.codemize.gestaocondominio.dto.DespesaRequest;
 import online.codemize.gestaocondominio.service.DespesaService;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,10 +9,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("api/despesa")
 public class DespesaContoller {
 
-    private DespesaService service;
+    private final DespesaService service;
 
     @PostMapping
     public void cadastrarDespesa(@RequestBody DespesaRequest request){
---
+++ src/main/java/online/codemize/gestaocondominio/service/DespesaServiceImpl.java
@@ -0,0 +1,13 @@
+package online.codemize.gestaocondominio.service;
+
+import online.codemize.gestaocondominio.dto.DespesaRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DespesaServiceImpl implements DespesaService {
+
+    @Override
+    public void cadastrar(DespesaRequest request) {
+        System.out.println("falta implementar");
+    }
+}
