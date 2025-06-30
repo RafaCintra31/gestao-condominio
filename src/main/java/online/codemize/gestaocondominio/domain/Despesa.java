d303835c154206894c1b853530ae6cb71e6de1fd
rafa cintra <rafa.cintrati@gamil.com>
2025-06-30 19:26:40 -0300

    atualicacao do projeto gestao condominio

--- src/main/java/online/codemize/gestaocondominio/domain/Despesa.java
+++ src/main/java/online/codemize/gestaocondominio/domain/Despesa.java
@@ -1,23 +1,53 @@
 package online.codemize.gestaocondominio.domain;
 
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 import online.codemize.gestaocondominio.domain.enums.StatusDespesa;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+/*
+    jatkarta = JPA
+    Vamos usar uma lib chamada Hibernate (mapeamneto de banco de dados no java)
+*/
 
 @Getter
 @Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "despesas")
 public class Despesa {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @Column(nullable = false)
     private String categoria;
+
+    @Column(nullable = false)
     private String descricao;
+
+    @Column(name = "valor_original", nullable = false)
     private Double valorOriginal;
+
+    @Column(name = "valor_pago")
     private Double valorPago;
-    private LocalDate dataCriacao;
-    private LocalDate dataPagamento;
+
+    @Column(name = "data_vencimento", nullable = false)
     private LocalDate dataVencimento;
+
+    @Column(name = "data_pagamento")
+    private LocalDate dataPagamento;
+
+    @Column(name = "data_criacao")
+    private LocalDateTime dataCriacao;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
     private StatusDespesa status;
 
 
--- src/main/java/online/codemize/gestaocondominio/dto/DespesaRequest.java
+++ src/main/java/online/codemize/gestaocondominio/dto/DespesaRequest.java
@@ -1,21 +1,9 @@
 package online.codemize.gestaocondominio.dto;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-
-import java.time.LocalDate;
-
-@Getter
-@Setter
-@ToString
-public class DespesaRequest {
-
-    private String categoria;
-    private String descricao;
-    private Double valorOriginal;
-    private Double valorPago;
-    private String dataPagamento;
-    private String dataVencimento;
-
+public record DespesaRequest(String categoria,
+                             String descricao,
+                             Double valorOriginal,
+                             Double valorPago,
+                             String dataPagamento,
+                             String dataVencimento){
 }
