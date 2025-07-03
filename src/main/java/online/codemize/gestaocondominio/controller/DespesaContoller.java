c6c6a2052c41a41d7f3ecca679a9640367f81a15
rafa cintra <rafa.cintrati@gamil.com>
2025-07-03 16:23:15 -0300

    projeto atualizado

--- src/main/java/online/codemize/gestaocondominio/controller/DespesaContoller.java
+++ src/main/java/online/codemize/gestaocondominio/controller/DespesaContoller.java
@@ -1,12 +1,14 @@
 package online.codemize.gestaocondominio.controller;
 
 import lombok.RequiredArgsConstructor;
+import online.codemize.gestaocondominio.domain.Despesa;
 import online.codemize.gestaocondominio.dto.DespesaRequest;
 import online.codemize.gestaocondominio.service.DespesaService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,8 +18,21 @@ public class DespesaContoller {
     private final DespesaService service;
 
     @PostMapping
-    public void cadastrarDespesa(@RequestBody DespesaRequest request){
+    @ResponseStatus(HttpStatus.CREATED)
+    public void cadastrarDespesa(@RequestBody DespesaRequest request) {
         service.cadastrar(request);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Despesa> listarDespesas(){
+        return service.listar();
+    }
+
+    @GetMapping(value = "{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public Despesa getDespesaPorId(@PathVariable Long id){
+        return service.buscarPorId(id);
     }
 
 }
